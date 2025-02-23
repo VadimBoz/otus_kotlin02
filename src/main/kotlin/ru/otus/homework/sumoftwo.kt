7f1a57a061c6d088cb5bdab62b3830e27e293ce0
VadimBoz <werbel34@mail.ru>
2025-02-23 20:23:55 +0300

    kotlin02_hw refactoring2

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -13,7 +13,7 @@ package ru.otus.homework
  */
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    for (i in numbers.indices) {
+    for (i in 0 until numbers.size - 1) {
         for (j in i + 1 until numbers.size) {
             if (numbers[i] + numbers[j] == target) return intArrayOf(i, j)
         }
