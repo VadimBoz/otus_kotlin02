7dcc0d2ff43531981eff07c0f54f0f627099b0f7
VadimBoz <werbel34@mail.ru>
2025-02-23 18:17:20 +0300

    kotlin02_hw

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,22 @@
 package ru.otus.homework
 
+/**
+Функция `sumOfTwo` принимает массив целых чисел `numbers`, целое число `target`.
+
+Реализуйте тело функции таким образом, чтобы она возвращала массив целых чисел, состоящий из
+индексов элементов массива numbers так,
+чтобы сумма значений чисел находящихся на этих индексах была равна числу target.
+
+Достаточно найти только одну пару чисел, удовлетворяющих условию.
+
+Если таких чисел нет, то выбросьте исключение `IllegalArgumentException`
+ */
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    for (i in numbers.indices) {
+        for (j in i + 1 until numbers.size) {
+            if (numbers[i] + numbers[j] == target) return intArrayOf(i, j)
+        }
+    }
+    throw IllegalArgumentException()
 }
